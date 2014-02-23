23de38c9e1da6b924fea371a9ce8c14d9f818534
howardchi <howardchi@fougood.com>
2014-02-23 13:30:13 +0800

    done

--- test.js
+++ test.js
@@ -13,32 +13,33 @@ describe('get tsv records and fieldname', function() {
 	})
 })
 
-// describe('tsv to json', function() {
+describe('tsv to json', function() {
 
-// 	it('should convert tsv to json', function() {
-// 		tsv2json({
-// 			input: './sample/sample.tsv',
-// 			output: null
-// 		}, function(err, result) {
-// 			should.not.exist(err)
-// 			result.should.be.an.instanceOf(Object)
-// 		})
-// 	})
+	it('should convert tsv to json', function(cb) {
+		tsv2json({
+			input: './sample/sample.tsv',
+			output: null
+		}, function(err, result) {
+			should.not.exist(err)
+			result.should.be.an.instanceOf(Object)
+			cb()
+		})
+	})
 
-// 	it('should convert tsv to json file', function() {
-// 		tsv2json({
-// 			input: './sample/sample.tsv',
-// 			output: './sample/test.json'
-// 		}, function(err, result) {
-// 			should.not.exist(err)
-// 			result.should.be.an.instanceOf(Object)
-// 		})
+	it('should convert tsv to json file', function() {
+		tsv2json({
+			input: './sample/sample.tsv',
+			output: './sample/test.json'
+		}, function(err, result) {
+			should.not.exist(err)
+			result.should.be.an.instanceOf(Object)
+		})
 
-// 	})
+	})
 
-// 	it('should read file in test.json', function() {
-// 		var exist = fs.existsSync('./sample/test.json')
-// 		exist.should.be.true;
-// 	})
+	it('should read file in test.json', function() {
+		var exist = fs.existsSync('./sample/test.json')
+		exist.should.be.true;
+	})
 
-// })
+})
